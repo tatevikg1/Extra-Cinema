d21d04258682a0f337a6473b111f6713023f34a2
ta <tatevikg1@gmail.com>
2021-09-23 12:30:52 +0400

    start seance

--- src/components/dashboard/StartSession.vue
+++ src/components/dashboard/StartSession.vue
@@ -1,28 +1,16 @@
 <template>
   <div class="form">
-    <div class="row">
+    <div v-for="seance in seances" :key="seance.id" class="row">
       <div class="col">
-        <h2 class="text-white text-medium">Довод</h2>
-        <router-link to="somePath" class="button text-white text-semi-bold"
+        <h2 class="text-white text-medium">{{ seance.film.title }}</h2>
+        <router-link :to="`test-seance/${seance.id}`" class="button text-white text-semi-bold"
           ><img :src="AirPlayIcon" alt="" /> Тестовый режим</router-link
         >
-        <router-link to="somePath" class="button text-white text-semi-bold"
+        <router-link :to="`seance/${seance.id}`" class="button text-white text-semi-bold"
           ><img :src="DesktopMacIcon" alt="" /> Основной показ</router-link
         >
       </div>
-      <img :src="PlayerExample" class="player" alt="" />
-    </div>
-    <div class="row">
-      <div class="col">
-        <h2 class="text-white text-medium">Довод</h2>
-        <router-link to="somePath" class="button text-white text-semi-bold"
-          ><img :src="AirPlayIcon" alt="" /> Тестовый режим</router-link
-        >
-        <router-link to="somePath" class="button text-white text-semi-bold"
-          ><img :src="DesktopMacIcon" alt="" /> Основной показ</router-link
-        >
-      </div>
-      <img :src="PlayerExample" class="player" alt="" />
+      <img :src="`${baseUrl}/storage/${seance.film.image}`" class="player" alt="no image" />
     </div>
     <div class="row buttons">
       <button class="text-semi-bold">
@@ -41,6 +29,7 @@ import AirPlayIcon from "@/assets/images/dashboard/airplay.svg";
 import DesktopMacIcon from "@/assets/images/dashboard/desktop_mac.svg";
 import VideocamIcon from "@/assets/images/dashboard/videocam.svg";
 import VolumeIcon from "@/assets/images/dashboard/volume.svg";
+import axios from 'axios';
 export default {
   data() {
     return {
@@ -49,7 +38,26 @@ export default {
       DesktopMacIcon,
       VideocamIcon,
       VolumeIcon,
+      seances: [],
+      baseUrl: process.env.VUE_APP_API_URL
     };
+  },
+  mounted() {
+    this.getSeances();
+  },
+  methods: {
+    getSeances() {
+      axios
+        .post(process.env.VUE_APP_API_URL + "/api/seances", {
+          token: this.$store.getters.getAuthToken,
+        })
+        .then((res) => {
+          this.seances = res.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
   },
 };
 </script>
---
+++ src/views/cinema/Seance.vue
@@ -0,0 +1,89 @@
+<template>
+  <video
+    v-if="videoUrl"
+    ref="player"
+    :src="`${baseUrl}/storage/${videoUrl}`"
+    width="100%"
+    height="100%"
+    frameborder="0"
+    autoplay
+    webkitallowfullscreen
+    mozallowfullscreen
+    allowfullscreen
+  ></video>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      baseUrl: process.env.VUE_APP_API_URL,
+      videoUrl: null,
+      duration: null,
+      seconds: 60,
+    };
+  },
+  created() {
+    this.getUrl();
+  },
+  updated() {
+    if (this.$route.name != "ec-seance") {
+      this.playPart();
+    }
+  },
+  methods: {
+    getUrl() {
+      axios
+        .post(
+          process.env.VUE_APP_API_URL + "/api/films/" + this.$route.params.id,
+          { token: this.$store.getters.getAuthToken }
+        )
+        .then((res) => {
+          this.videoUrl = res.data.file;
+          this.duration = res.data.duration;
+        })
+        .catch((err) => {
+          if (err.response.status == 401) {
+            this.$store.commit("deleteAuthToken");
+          }
+        });
+    },
+    playPart() {
+      const media = document.querySelector("video");
+      media.currentTime = Math.floor(Math.random() * (this.duration - 1)) * 60;
+      this.countDownTimer();
+      // console.log(Math.floor(Math.random() * (this.duration - 1)) * 60, this.duration)
+    },
+    countDownTimer() {
+      if (this.seconds > 0) {
+        setTimeout(() => {
+          this.seconds = this.seconds - 1;
+          this.countDownTimer();
+        }, 1000);
+      } else {
+        this.$router.push("/cinema/dashboard").catch(() => {});
+      }
+    },
+  },
+};
+</script>
+
+<style scoped>
+video {
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  border: none;
+  margin: 0;
+  padding: 0;
+  overflow: hidden;
+  z-index: 999999;
+}
+</style>
+
